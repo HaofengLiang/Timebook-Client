7f03f7b0feb3425dfe73d3b5a05b661eaa1180cf
HaofengLiang <31834884+HaofengLiang@users.noreply.github.com>
2023-05-02 17:17:24 -0400

    Add  simple login logic.

---
+++ src/components/calendar/Calendar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Calendar() {
+    return (
+        <div className="App">
+            <h1>Calendar</h1>
+        </div>
+    );
+}
+
+export default Calendar;
