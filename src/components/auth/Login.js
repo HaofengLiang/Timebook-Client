80d5db8fb9a2f1bb7c03a9e56857386e30d7393f
HaofengLiang <31834884+HaofengLiang@users.noreply.github.com>
2023-05-02 20:24:50 -0400

    Add error handles.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,19 +1,35 @@
 import "./Login.css"
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button, FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
 
 export default function Login({ onLogin }) {
     const emailInputRef = useRef();
+    const [error, setError] = useState(true);
+
     const loginHandler = () => {
+        if (!emailInputRef.current.value.includes('@')) {
+            setError(true);
+            return;
+        }
+
+        setError(false);
         onLogin(emailInputRef.current.value);
     };
 
     return (
         <div className="LoginPage">
             <FormControl className="LoginForm" >
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                <Input inputRef={emailInputRef} id="my-input" aria-describedby="my-helper-text" />
-                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                <InputLabel htmlFor="email-input">Email address</InputLabel>
+                <Input
+                    inputRef={emailInputRef}
+                    error={error}
+                    id="email-input"
+                    aria-describedby="my-helper-text"
+                />
+                {error ?
+                    <FormHelperText error id="email-helper-text-error">Please enter a valid email.</FormHelperText> :
+                    <FormHelperText id="email-helper-text">We'll never share your email.</FormHelperText>
+                }
                 <Button variant="contained" color="primary" onClick={loginHandler}>Login</Button>
             </FormControl>
         </div>
