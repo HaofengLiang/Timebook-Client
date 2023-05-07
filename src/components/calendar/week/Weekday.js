7920c21585423198ff912339be00125fceb04107
HaofengLiang <31834884+HaofengLiang@users.noreply.github.com>
2023-05-07 15:48:50 -0400

    Use list instead of table cell for day view.

--- src/components/calendar/week/Weekday.js
+++ src/components/calendar/week/Weekday.js
@@ -1,27 +1,28 @@
 import "./Weekday.css"
-import { Box, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import moment from 'moment';
 
+// q: why the list key is repeated?
+// a: https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
 export default function Weekday({ events, day, onDateTimeSelect }) {
     const hrs = 24;
     const minsPerSection = 30;
     const numOfSections = hrs * (60 / minsPerSection);
     const listItems = [...Array(numOfSections)].map((_, i) => {
         const dateTime = moment(day).startOf('day').add(i * minsPerSection, 'minutes');
-        return (
-            <ListItem key={i} className='Item'>
-                {day
-                    ? <ListItemButton onClick={() => onDateTimeSelect(dateTime)}></ListItemButton>
-                    : <ListItemText primary={dateTime.format('h:mm A')} />}
-            </ListItem>);
+        return day ?
+            <ListItem key={dateTime.format('MM-DD-yyyy-hh-mm-A') + "-list-item"} className='datetimeItem' >
+                <ListItemButton onClick={() => onDateTimeSelect(dateTime)} className="datetimeButton"></ListItemButton>
+            </ListItem > :
+            <ListItem key={dateTime.format('hh-mm-A') + "list-item"} className='datetimeItem datetimeHeader'>
+                <ListItemText primary={dateTime.format('hh:mm A')} />
+            </ListItem>
     });
 
     return (
-        <Box sx={{ height: '100%', width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <List className="dayBody">
-                {listItems}
-            </List>
-        </Box>
+        <List className="dayBody">
+            {listItems}
+        </List>
     );
 
 };
