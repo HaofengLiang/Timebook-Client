8ccba57cefc48e4c745408dfb564248fbbb54915
HaofengLiang <31834884+HaofengLiang@users.noreply.github.com>
2023-05-07 21:41:32 -0400

    Display events.

--- src/components/calendar/week/WeekBody.js
+++ src/components/calendar/week/WeekBody.js
@@ -1,18 +1,19 @@
 import { TableCell, TableBody, TableRow } from "@mui/material";
 import React from 'react';
 import './Weekday.css';
-import Weekday from "./Weekday";
+import WeekDay from "./Weekday";
 
 export default function WeekBody({ events, days, onDateTimeSelect }) {
+    console.log(events);
     return (
         <TableBody>
             <TableRow key={"datetime"} className='Item'>
                 <TableCell key={'datetime-header'} className="tableCell">
-                    <Weekday isHeader />
+                    <WeekDay isHeader />
                 </TableCell>
                 {days.map((day) =>
                     <TableCell key={day.format("MM-DD-yyyy") + '-datetime-body'} className="tableCell" align="center">
-                        <Weekday day={day} events={events} onDateTimeSelect={onDateTimeSelect} />
+                        <WeekDay day={day} events={events?.filter(event => event.start.isSame(day, "day"))} onDateTimeSelect={onDateTimeSelect} />
                     </TableCell>)
                 }
             </TableRow >
