ec1723d065ebde814a8d51c6e759fcec4444c1d8
Lin1404 <78375725+Lin1404@users.noreply.github.com>
2023-05-29 14:14:46 -0400

    Events render, style

--- src/components/calendar/week/Weekday.js
+++ src/components/calendar/week/Weekday.js
@@ -1,21 +1,9 @@
 import "./Weekday.css"
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
-import { useSelector } from 'react-redux'
 import moment from 'moment';
 import Events from "../../event/Events";
 
-// function Event({ event }) {
-//     return (
-//         <div className="event">
-//             <div className="eventTitle">{event.title}</div>
-//             <div className="eventTime">{event.startDateTime.format('hh:mm A')} - {event.endDateTime.format('hh:mm A')}</div>
-//             <div className="eventDescription">{event.description}</div>
-//         </div>
-//     );
-// };
-
 export default function WeekDay({ day, onEventSelect, isHeader }) {
-    // const events = useSelector((state) => state.events.value.filter(item => item.startDateTime.isSame(day, "day")));
     const heightPerRowInVH = 5;
     const hrs = 24;
     const minsPerSection = 30;
@@ -32,19 +20,9 @@ export default function WeekDay({ day, onEventSelect, isHeader }) {
             endDateTime: moment(dateTime).add(minsPerSection, 'minutes'),
             description:'',
             priority:0
-        };
-
-        // const event = events?.find(event =>
-        //     event.startDateTime.isSame(dateTime) || event.startDateTime.isBetween(dateTime, moment(dateTime).add(minsPerSection, 'minutes'))
-        // );  
+        }; 
         
         let rowSpan = 1;
-        // if (event) {
-        //     const startDateTime = moment(event.startDateTime);
-        //     const endDateTime = moment(event.endDateTime);
-        //     const minsDiff = endDateTime.diff(startDateTime, 'minutes');
-        //     rowSpan = minsDiff === 0 ? 1 : Math.ceil(minsDiff / minsPerSection);
-        // }
 
         listItems.push(
             isHeader ?
@@ -64,8 +42,6 @@ export default function WeekDay({ day, onEventSelect, isHeader }) {
         <div>
             <List className="dayBody">
                 {listItems}
-            </List>
-            <List className="eventsList">
                 <Events day={day} onEventSelect={onEventSelect}/>
             </List>
         </div>
