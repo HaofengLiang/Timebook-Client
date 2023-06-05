0ea1a13be5238eca7943aecd39646f408091e19d
weixiang237 <87122779+weixiang237@users.noreply.github.com>
2023-06-05 10:24:21 -0400

    fix css in main mui components

--- src/components/sidebar/MuiComponents.js
+++ src/components/sidebar/MuiComponents.js
@@ -1,20 +1,20 @@
-import { styled } from "@mui/material/styles";
-import MuiAppBar from "@mui/material/AppBar";
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
 
 export const drawerWidth = 240;
 
-export const Main = styled("main", drawerWidth, {
-  shouldForwardProp: (prop) => prop !== "open",
+export const Main = styled('main', drawerWidth, {
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
   flexGrow: 1,
-  padding: theme.spacing(1),
-  transition: theme.transitions.create("margin", {
+  padding: theme.spacing(0),
+  transition: theme.transitions.create('margin', {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   marginLeft: `-${drawerWidth}px` - 20,
   ...(open && {
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -23,26 +23,26 @@ export const Main = styled("main", drawerWidth, {
 }));
 
 export const AppBar = styled(MuiAppBar, drawerWidth, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-export const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+export const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
+  justifyContent: 'flex-end',
 }));
